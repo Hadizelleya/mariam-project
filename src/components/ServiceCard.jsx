b62e7cc593285765ae2e765c1f09b843cd638aa0
HADYZELLEYA\hadi_ <hadi_scorpion95@hotmail.com>
2025-04-22 19:30:37 +0300

    bug fixes

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
-import categoryImage from "../assets/wood-category-1.jpg";
-export default function ServiceCard({ icon, title, subtitle, description }) {
+
+export default function ServiceCard({ image, title, subtitle, description }) {
   return (
     <div className="flex flex-col items-center justify-center hover:scale-105 transition-all duration-300 group  shadow-lg h-full  rounded-t-lg hover:shadow-xl">
-      <div className="flex flex-col items-center justify-center gap-5 p-5 rounded-t-lg cursor-pointer bg-white w-full h-full">
-        <div>
+      <div className="flex flex-col items-center justify-start gap-5  rounded-t-lg cursor-pointer bg-white w-full h-full">
+        <div className="w-full">
           <img
-            src={categoryImage}
+            src={image}
             alt="category image"
-            className="rounded-md"
+            className="rounded-md w-full h-[400px]"
           />
         </div>
-        <h1 className="text-2xl font-bold text-center">{title}</h1>
-        <h2 className="text-xl font-semibold text-center text-(--color-primary)/70">
-          {subtitle}
-        </h2>
-        <p className="text-center text-gray-600">{description}</p>
+        <div className="flex flex-col items-cneter justify-center gap-2 p-4">
+          <h1 className="text-2xl font-bold text-center">{title}</h1>
+          <h2 className="text-xl font-semibold text-center text-(--color-primary)/70">
+            {subtitle}
+          </h2>
+          <p className="text-center text-gray-600">{description}</p>
+        </div>
       </div>
       <div className="flex items-center justify-between w-full px-4 py-2 bg-(--color-primary) text-white rounded-b-lg shadow-md hover:bg-(--color-black)/90 hover:text-(--color-white) transition-all duration-300 cursor-pointer">
         <p className="text-lg font-semibold">Check More</p>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
-import { FaCloud } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
-import { IoTv } from "react-icons/io5";
-import { HiServer } from "react-icons/hi2";
-
+import categoryImage from "../assets/wood-category-1.jpg";
+import categoryImage2 from "../assets/wood-category-2.jpg";
+import categoryImage3 from "../assets/wood-category-3.jpg";
+import categoryImage4 from "../assets/wood-category-4.jpg";
 export default function Services() {
   return (
     <div>
@@ -14,25 +13,25 @@ export default function Services() {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           <ServiceCard
-            icon={<FaCloud />}
+            image={categoryImage}
             title="Cloud Services"
             subtitle="Cloud Solutions"
             description="We offer a range of cloud services to help you scale your business."
           />
           <ServiceCard
-            icon={<IoMail />}
+            image={categoryImage2}
             title="Email Services"
             subtitle="Email Solutions"
             description="Secure and reliable email services for your business."
           />
           <ServiceCard
-            icon={<IoTv />}
+            image={categoryImage3}
             title="TV Services"
             subtitle="Entertainment Solutions"
             description="High-quality TV services for your entertainment needs."
           />
           <ServiceCard
-            icon={<HiServer />}
+            image={categoryImage4}
             title="Server Solutions"
             subtitle="Hosting Solutions"
             description="Reliable server solutions to host your applications."
